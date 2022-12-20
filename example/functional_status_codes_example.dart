3590bec2dca029f6f301f56fb86e7d36f8451056
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2022-12-20 20:50:27 +0100

    feat: added github dir and adjusted analyzer

--- example/functional_status_codes_example.dart
+++ example/functional_status_codes_example.dart
@@ -1,6 +1,6 @@
 import 'package:functional_status_codes/functional_status_codes.dart';
 
 void main() {
-  var awesome = Awesome();
+  final awesome = Awesome();
   print('awesome: ${awesome.isAwesome}');
 }
