23d35c4fc890d01b8dc93409738e4fc61712e94f
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2022-12-20 19:39:53 +0100

    Initial commit

---
+++ example/functional_status_codes_example.dart
@@ -0,0 +1,6 @@
+import 'package:functional_status_codes/functional_status_codes.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/functional_status_codes.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library functional_status_codes;
+
+export 'src/functional_status_codes_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
